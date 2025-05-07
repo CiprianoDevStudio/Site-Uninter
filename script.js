59581c63789b0fd685cb6652afbb7f2fbab8ac9b
Cipriano, the Dev <ciprianoads@gmail.com>
2025-05-07 00:53:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mensagem(){
+    document.getElementById("botao").innerHTML = `<p>Desempenho é minha opção. E tá na cara: não paro até ser melhor do que ontem. <span style="color: red" >Forza Ferrari</span></p>`;
+}
